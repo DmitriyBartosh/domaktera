b3c874d480cb1e634e611e66d63e9f52e83224c3
DmitriyBartosh <Bartosh92@mail.ru>
2021-08-12 00:42:32 +0700

    add

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       resolve: "gatsby-source-wordpress",
       options: {
-        url: "https://domaktera24.ru/csm/graphql",
+        url: process.env.WPGRAPHQL_URL || `https://domaktera24.ru/csm/graphql`,
         protocol: "https",
       },
     },
